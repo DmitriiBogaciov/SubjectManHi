d13cbcf53c79d804be757fe9f84e52d0ee4349c8
Dmitrii Bogaciov <dmitrii.bogaciov@gmail.com>
2024-01-18 13:26:01 +0100

    more 1

--- src/components/visual-component/programme-modal.js
+++ src/components/visual-component/programme-modal.js
@@ -41,7 +41,7 @@ const StudyProgrammeModal = ({ show, handleClose, handleCreateProgramme, studyPr
       console.log(newFormData)
       setFormData(newFormData)
     }
-  }, [studyProgramme, formData])
+  }, [studyProgramme, formData, subjects])
 
   useEffect(() => {
 
